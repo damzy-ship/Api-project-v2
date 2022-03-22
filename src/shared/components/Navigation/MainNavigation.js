ee37fd267fdea9e031acb34a896e05b182b48c78
Damilola Aguda <63460192+damzy-ship@users.noreply.github.com>
2022-03-22 21:00:06 +0100

    Initial commit

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./MainNavigation.css";
+import MainHeader from "./MainHeader";
+import { Link } from "react-router-dom";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import BackDrop from "../UIElements/Backdrop";
+
+const MainNavigation = () => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const openDrawer = () => setDrawerIsOpen(true);
+  const closeDrawer = () => setDrawerIsOpen(false);
+  return (
+    <>
+      {drawerIsOpen && <BackDrop onClick={closeDrawer}/>}
+      {drawerIsOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button onClick={openDrawer} className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/"> YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
+  );
+};
+
+export default MainNavigation;
